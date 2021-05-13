eb295c55fb61e81a0412567aefca492988acceb4
Niklas Babendererde <niklas.babendererde@stud.tu-darmstadt.de>
2021-05-13 14:18:21 +0200

    Added xacro_processor

---
+++ scripts/xacroProcessor.py
@@ -0,0 +1,70 @@
+import os
+from sys import settrace
+import time
+
+# XML-Stuff
+import xacro
+from xml.dom import minidom
+import subprocess
+
+def save_xml(file, doc):
+    try:
+        out = open(file, 'w')
+        out.write(doc.toprettyxml(indent='  '))
+        out.close()
+    except IOError as e:
+        raise FileExistsError("Failed to open output:", exc=e)
+
+
+def read_xml(file):
+    try:
+        doc = minidom.parse(file)
+        return doc
+    except IOError as e:
+        raise FileExistsError("Failed to open output:", exc=e)
+
+def replace_value(doc, tag, value):
+    '''
+        doc: xml-document to parsed through
+        tag: name of node, where parameter has to be altered
+        value: value to be written onto the node-parameter
+    '''
+    nodes = doc.getElementsByTagName(tag)
+    for node in nodes:
+        print(node)
+
+
+# def remove_tag_velocity_decay(doc):
+#     nodes = doc.getElementsByTagName('velocity_decay')
+#     nodes[0].parentNode.removeChild(nodes[0])
+#     return doc
+
+def load_doors(door_path, **kwargs):
+    args = [os.path.join(os.getcwd(), door_path)] #
+
+    for key, value in kwargs.items():
+        args.append(key + ":=" + str(value))
+    print(args)
+    
+    opts, input_file_name = xacro.process_args(args)
+    doc = xacro.process_file(input_file_name, **vars(opts))
+    replaced_path="".join(door_path.split(".")[:-1])+"_replaced.xacro"
+    print(replaced_path)
+    save_xml(replaced_path, doc)
+
+
+
+def load_handles(handle_path, **kwargs):
+    pass
+
+def main():
+
+    xacro_path = 'path/to/xacros/'
+    xacro_path = os.path.abspath('../urdf_xacro/plane.xacro')
+    # load xacro file(s)
+    print(xacro_path)
+    print("abs poath: "+os.path.abspath(xacro_path))
+    load_doors(xacro_path, plane_file='../src/urdf_xacro/door2.obj', parent='world', size_x=1.0, size_y=1.0, size_z=1.0)
+
+if __name__ == '__main__':
+    main()
