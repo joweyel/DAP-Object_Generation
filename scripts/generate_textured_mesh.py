f9062af11db83bd8297cd49e688f222b0921e7a5
Johannes Weyel <johannes.weyel@stud.tu-darmstadt.de>
2021-05-21 01:02:43 +0200

    data generator process

---
+++ scripts/generate_textured_mesh.py
@@ -0,0 +1,40 @@
+import os
+from natsort import natsorted
+
+def main():
+    root = os.getcwd()
+
+    # get the directory with the data generation script (door/handle)
+    door_dir = '../data/objs/pieces/doors/'
+    print('Door dir: ', os.getcwd())
+    os.chdir(door_dir)
+    script = 'generate_textured_doors.py'
+    print('Current dir: ', os.getcwd())
+    blend_files = natsorted(os.listdir('./blend/'))
+    print('blend_files: ', blend_files)
+
+    for bf in blend_files:
+        print('Door: ', bf)
+        bash_query = 'blender -b ' + 'blend/' + bf + ' --python ' + script
+        print(bash_query)
+        os.system(bash_query)
+
+    return
+    ## TODO: implementin the same thing for handles ##
+    os.chdir(root) # back to the roots, namely the "scripts"-folder
+    handle_dir = '../data/objs/pieces/hadles/'
+    print('Current dir: ', os.getcwd())
+    os.chdir(handle_dir)
+    script = 'generate_textured_obj.py'
+    print('Handle dir: ', os.getcwd())
+    blend_files = natsorted(os.listdir('./blend/'))
+    print('blend_files: ', blend_files)
+
+    for bf in blend_files:
+        print('Processing: ', bf)
+        bash_query = 'blender -b ' + './blend/' + bf + ' --python ' + script
+        os.system(bash_query)
+
+
+if __name__ == '__main__':
+    main()
