bcd16e39cbea6ad92f02c9dd619c3f784fbad319
Johannes Weyel <johannes.weyel@stud.tu-darmstadt.de>
2021-06-28 21:06:26 +0200

    Basic Dataloader for Doordata; has to be adapted to used DNN

---
+++ data/train_data/DoorDataloader.py
@@ -0,0 +1,115 @@
+import os
+import json
+import numpy as np
+import torch
+import torch.utils.data
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.lines as mlines
+import pandas as pd
+
+
+def getAABB(img, bbox, ax=None, color='r'):
+    x_min, y_min = bbox[0, :]
+    x_max, y_max = bbox[1, :]
+    if ax is None:
+        _, ax = plt.subplots()
+    rect = patches.Rectangle((x_min, y_min), x_max-x_min, y_max-y_min, 
+                             linewidth=1, edgecolor=color, facecolor='none')
+    ax.add_patch(rect)
+    return ax
+
+def plotAABB(img, ax, idx):
+    ax.imshow(img)
+    # plt.savefig('test/bbox_img_{}.jpg'.format(idx), dpi=200)
+    plt.show()
+    
+
+
+class DoorDataset(torch.utils.data.Dataset):
+    def __init__(self, root, data_file, train, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.train = train
+        # self.Imgs = None
+        # self.depthImgs = None
+        # self.segImgs = None
+        self.img_dir = os.path.join(root, 'images')
+        self.feature_dir = os.path.join(root, 'features')
+
+        # image prefixes: '_rgb.png', '_depth.png' and '_seg.png' get appended in __getitem__
+        # json-name is the same as the prefix
+        self.img_prefix = sorted(['_'.join(img.split('_')[:-1]) for img in os.listdir(self.img_dir)])
+
+
+    def __getitem__(self, idx, debug=False):
+
+        ## get the base-path and append relevant endings to load
+        img_base = os.path.join(self.root, 'images', self.img_prefix[idx])
+        rgb_path = img_base + '_rgb.png'
+        depth_path = img_base + '_depth.png'
+        seg_path = img_base + '_seg.png'
+
+        ## load rgb-image, depth-image and segmentation image
+        rgb_img = plt.imread(rgb_path)
+        depth_img = plt.imread(depth_path)
+        seg_img = plt.imread(seg_path)
+
+        ## get feature-information corresponding to the images
+        feature_path = os.path.join(self.root, 'features', self.img_prefix[idx]) + '.json'
+        with open(feature_path, 'r') as json_file:
+            features = json.load(json_file)
+
+        ## get door bounding box from segmentation-mask
+        cls = np.unique(seg_img)
+        n_cls = len(cls)
+        door_cls = cls[1]
+        y_idx, x_idx = np.where(seg_img == door_cls)
+        x_min, x_max = x_idx.min(), x_idx.max()
+        y_min, y_max = y_idx.min(), y_idx.max()
+        
+        ## get handle buonding box from saved features
+        bbox_door = np.array([[x_min, y_min], [x_max, y_max]])
+        # bbox_door = np.array([features['object']['min'], features['object']['max']])
+        bbox_handle = np.array([features['handle']['min'], features['handle']['max']])
+        
+        ## load the axis information (maybe plot)
+        axis = features['axis']
+        x0, y0 = axis[0]
+        x1, y1 = axis[1]
+
+
+        if debug:
+            # _, ax = plt.subplots(1, 3)
+            # ax[0].imshow(rgb_img); ax[0].set_title('rgb')
+            # ax[1].imshow(depth_img); ax[1].set_title('depth')
+            # ax[2].imshow(seg_img); ax[2].set_title('seg')
+            # plt.show()
+
+            ax = getAABB(rgb_img, bbox_door)
+            ax = getAABB(rgb_img, bbox_handle, ax, color='b')
+            l = mlines.Line2D([x0, x1], [y0, y1], color='b',
+                              linewidth=3)
+            ax.add_line(l)
+            plotAABB(rgb_img, ax, idx)
+            
+
+
+        target = dict()
+        target['rgb'] = torch.tensor(rgb_img)
+        target['depth'] = torch.tensor(depth_img)
+        target['seg'] = torch.tensor(seg_img)
+        target['axis'] = 0
+        return target
+
+    def __len__(self):
+        return len(self.img_prefix)
+
+def main():
+    dds = DoorDataset(root='./', train=True, data_file=None, transforms=None)
+    for i in range(dds.__len__()):
+        dds.__getitem__(i)
+    print('len = ', dds.__len__())
+
+if __name__ == '__main__':
+    main()
