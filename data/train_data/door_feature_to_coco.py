76a639c43f9dbe51953c103057f4021cb2254f31
niklasb2015 <niklas.babendererde@stud.tu-darmstadt.de>
2021-08-11 15:27:39 +0200

    Using seg img for more precise plane bbs

--- data/train_data/door_feature_to_coco.py
+++ data/train_data/door_feature_to_coco.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import json
+import png
 
 import numpy as np
 
 def main(path):
     features = [f for f in os.listdir(path) if f.endswith('json')]
-    img_path = 'data/train_data/images'
+    img_path = path.replace("/features","/images")#'../DAP/DAP-Object_Generation/data/train_data/images'
     feature_paths = []
     rgb_paths = []
     depth_paths = []
@@ -21,18 +22,30 @@ def main(path):
 
     for f in features:
         base_name = '.'.join(f.split('.')[:-1])
-        feature_path = os.path.join(path, f)
-        # print(base_name, feature_path)
-        rgb_path = os.path.join(img_path, base_name) + '_rgb.png'
-        depth_path = os.path.join(img_path, base_name) + '_depth.png'
         seg_path = os.path.join(img_path, base_name) + '_seg.png'
         in_json = open(os.path.join(path, f))
         d=json.load(in_json)
-        plane_bb_min=d['object']['min']
-        plane_bb_max=d['object']['max']
         handle_bb_min=d['handle']['min']
         handle_bb_max=d['handle']['max']
 
+        #Implemented according to
+        im=png.Reader(seg_path)
+        direct=im.asDirect()
+        image_2d=np.vstack([arr for arr in direct[2]])
+        labelMap=np.reshape(image_2d, (direct[1], direct[0], direct[3]['planes']))
+        plane_id = np.unique(labelMap.reshape(-1, labelMap.shape[2]), axis=0)[1]
+        labelMask=(labelMap == plane_id).all(axis=2)
+        labelMask=np.expand_dims(labelMask, axis=2)
+        labelMask=labelMask.astype('uint8')
+
+        a=np.where(labelMask != 0)
+        xmin=int(min(a[1]))
+        ymin=int(np.min(a[0]))
+        xmax=int(np.max(a[1]))
+        ymax=int(np.max(a[0]))
+
+        plane_bbox=[xmin, ymin, xmax-xmin, ymax-ymin]
+        plane_area=int(np.sum(labelMask))
 
         image={
         'file_name': base_name+ '_rgb.png',
@@ -46,8 +59,8 @@ def main(path):
         plane_annotation = {
             'id': current_ann_id,
             'image_id': current_img_id,
-            'bbox': pb_to_coco_bb(plane_bb_min, plane_bb_max),
-            'area': 0,
+            'bbox': plane_bbox,
+            'area': plane_area,
             'iscrowd': 0,
             'category_id': 1,
             'segmentation': []
@@ -55,11 +68,12 @@ def main(path):
         current_ann_id += 1
         annotations.append(plane_annotation)
 
+        handle_area, handle_bbox = pb_to_coco_bb(handle_bb_min, handle_bb_max)
         handle_annotation = {
             'id': current_ann_id,
             'image_id': current_img_id,
-            'bbox': pb_to_coco_bb(handle_bb_min, handle_bb_max),
-            'area': 0,
+            'bbox': handle_bbox,
+            'area': handle_area,
             'iscrowd': 0,
             'category_id': 2,
             'segmentation': []
@@ -100,7 +114,7 @@ def pb_to_coco_bb(pb_bb_min, pb_bb_max):
     height= abs(pb_bb_min[1] - pb_bb_max[1])
     top_left_x=min(pb_bb_min[0], pb_bb_max[0])
     top_left_y = min(pb_bb_min[1], pb_bb_max[1])
-    return [top_left_x, top_left_y, width, height]
+    return width*height, [top_left_x, top_left_y, width, height]
 
 
 if __name__ == '__main__':
