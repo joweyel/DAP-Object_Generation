a965f499c01136a690105d9ed530d29b8e828bc3
niklasb2015 <niklas.babendererde@stud.tu-darmstadt.de>
2021-10-12 18:48:06 +0200

    Added comments, removed deprecated code

--- scripts/image_generation.py
+++ scripts/image_generation.py
@@ -14,7 +14,8 @@ from shapely.geometry import box, Polygon, LineString, Point
 import shapely
 import random
 
-
+# Clips bb to image dims to ensure stability and compute iou coverage
+# between bb(img coordinates) and image with helper function "iou_coverage"
 def check_coverage(bb, imwidth, imheight):
     im_boundary = np.copy(bb)
     im_boundary[0][0] = np.clip(im_boundary[0][0], 0, imwidth)
@@ -25,6 +26,7 @@ def check_coverage(bb, imwidth, imheight):
     iou = iou_coverage(bb, im_boundary)
     return iou
 
+# Checks if at least one of the two axis endpoints is in the image
 def rotation_axis_covered(axis_img, imwidth, imheight):
     if axis_img[0][0]<imwidth and axis_img[0][0]>0 and axis_img[0][1]<imheight and axis_img[0][1]>0:
         return True
@@ -32,6 +34,8 @@ def rotation_axis_covered(axis_img, imwidth, imheight):
         return True
     return False
 
+# Applying image weighted metric(edge coverage, plane coverage, handle coverage) 
+# and comparing the resulting score to min_score. Return True is score is >= min_score
 def good_image(plane_bb_img, handle_bb_img, axis_img, imwidth, imheight, min_ec=0.5, ec_weight=1.0, iou_weight=1.0, min_score=0.5):
     ec=edge_coverage(plane_bb_img, imwidth, imheight)
     iou_plane=check_coverage(plane_bb_img, imwidth, imheight)
@@ -40,6 +44,8 @@ def good_image(plane_bb_img, handle_bb_img, axis_img, imwidth, imheight, min_ec=
     score=(ec*ec_weight+iou_plane*iou_weight)/(ec_weight+iou_weight) if ec>=min_ec and axis_covered and handle_covered else 0
     return score>=min_score
 
+# Gets object bounding box in image coordinates (bb), image dimensions (imwidth, imheight)
+# and returns percentage (0-1) of the iou coverage between image and given bb.
 def iou_coverage(bb, image_bb):
     bb_edgepoints = bb_to_edgepoints(bb)
     im_edgepoints = bb_to_edgepoints(image_bb)
@@ -53,6 +59,7 @@ def iou_coverage(bb, image_bb):
     else: iou=0
     return iou
 
+# Check which percentage of edges of bb are covered in the image
 def edge_coverage(bb, imwidth, imheight):
     edges = bb_to_edgepoints(bb)
     edge_coverage=len(edges)
@@ -63,6 +70,7 @@ def edge_coverage(bb, imwidth, imheight):
             edge_coverage-=1
     return edge_coverage/len(edges)
 
+# Helperfunction to extract edge points of bb
 def bb_to_edgepoints(bb):
     bb_min_x = min(bb[0][0], bb[1][0])
     bb_max_x = max(bb[0][0], bb[1][0])
@@ -70,7 +78,9 @@ def bb_to_edgepoints(bb):
     bb_max_y = max(bb[0][1], bb[1][1])
     return [[bb_min_x, bb_min_y], [bb_min_x, bb_max_y], [bb_max_x, bb_max_y], [bb_max_x, bb_min_y]]
 
-
+# Applying transform from 3D world coorinate to 2D img coordinate
+# projectionMatrix and viewMatrix are of the type of PyBullet derived from OpenGL
+# and can be compute with p.computeProjectionMatrixFOV and p.computeViewMatrix
 def world_to_img(world_coord, projectionMatrix, viewMatrix, imwidth, imheight):
     K_x = np.asarray(projectionMatrix).reshape(4, 4).T  # [:3, :4]
     K_x = np.delete(K_x, 2, 0)
@@ -92,7 +102,8 @@ def world_to_img(world_coord, projectionMatrix, viewMatrix, imwidth, imheight):
 
     return [int(x_scaled[0]),int(y_scaled[1])]
 
-
+# Function for drawing bounding box in PB GUI, can be removed 
+# but maybe it's useful to visualize bbs for debugging
 def drawAABB(aabb):
     aabbMin = aabb[0]
     aabbMax = aabb[1]
@@ -140,7 +151,6 @@ def drawAABB(aabb):
     t = [aabbMax[0], aabbMax[1], aabbMin[2]]
     p.addUserDebugLine(f, t, [1, 1, 1])
 
-
 def find_files(filename, search_path):
     result = []
     # Wlaking top-down from the root
@@ -159,7 +169,8 @@ def get_json_data(file_path=None):
         data = json.load(file)
     return data
 
-
+# Saving RGBD images + corresponding json file with the features
+# (bb_door, bb_handle, rotation(axis) and axis_is_right)
 def generate_datapoint(file_name, bb_door, bb_handle, rotation, axis_is_right, json_path=None, **kwargs):
     '''
         Function: saves images and its corresponding features (in a json-file)
@@ -182,6 +193,7 @@ def generate_datapoint(file_name, bb_door, bb_handle, rotation, axis_is_right, j
     if 'rgb_img' in kwargs.keys():
         rgb_out = os.path.join(output_path, 'images/') + file_name.replace('.FORMAT', '_rgb.jpg')  # or jpg
         #plt.imsave(rgb_out,kwargs['rgb_img'], format='jpg', quality=90)
+        # cv2 imwrite takes quality parameter in position 2, >94 might be unstable according to cv2 doc
         cv2.imwrite(rgb_out, cv2.cvtColor(kwargs['rgb_img'],cv2.COLOR_BGR2RGB), [int(cv2.IMWRITE_JPEG_QUALITY), 94])
 
     if 'depth_img' in kwargs.keys():
@@ -196,6 +208,10 @@ def generate_datapoint(file_name, bb_door, bb_handle, rotation, axis_is_right, j
     with open(json_out, 'w') as f:
         json.dump(data, f, indent=4)
 
+# Extracts rotation axis from plane_bb and handle_bb
+# It's comparing handle-and plane center and determining if 
+# handle is right or left side. Then return axis_is_right boolean
+# and corner point of plane bb on the opposite side of handle as axis
 def get_rotation_axis(plane_bb, handle_bb):
     handle_center_y = (handle_bb[0][1] + handle_bb[1][1]) / 2
     plane_center_y = (plane_bb[0][1] + plane_bb[1][1]) / 2
@@ -208,13 +224,19 @@ def get_rotation_axis(plane_bb, handle_bb):
                 [(plane_bb[0][0] + plane_bb[1][0]) / 2, plane_bb[1][1], plane_bb[1][2]]]
     return axis_is_right, axis
 
+# Uses p.setjointMotorControl to move door to desired opening angle, 
+# door-urdf has to contain door joint at index 0
 def set_door_angle(goal_angle, obj):
     p.setJointMotorControl2(obj, 0, p.POSITION_CONTROL, goal_angle, force=999)
     while p.getJointState(obj, 0)[0] < goal_angle - 0.001:
         p.stepSimulation()
         time.sleep(1. / 240.)
 
-
+# Going along grid of follwoing camera parameters:
+# eye_xs, eye_ys, eye_zs: positions of camera in world cooridinates
+# tar_ys, tar_zs: view targets of camera (where to look at)=> camera orientation
+# door_angles: Determines how far to open the door
+# During iterating the grid image quality is checked and if good creating datapoint(img+feature json)
 def generate_data_imgs(obj, urdf_input, env_input, eye_xs, eye_ys, eye_zs, tar_ys, tar_zs, door_angles):
     for door_angle in door_angles:
         for eye_x in eye_xs:
@@ -246,7 +268,8 @@ def generate_data_imgs(obj, urdf_input, env_input, eye_xs, eye_ys, eye_zs, tar_y
                             set_door_angle(door_angle,obj)
                             plane_bb=p.getAABB(obj, linkIndex=0)
                             handle_bb=p.getAABB(obj, linkIndex=1)
-
+                            
+                            # both 3D world handle bb points
                             handle_bb_front_0=[max(handle_bb[0][0], handle_bb[1][0]), handle_bb[0][1], handle_bb[0][2]]
                             handle_bb_front_1 = [max(handle_bb[0][0], handle_bb[1][0]), handle_bb[1][1], handle_bb[1][2]]
 
@@ -264,7 +287,8 @@ def generate_data_imgs(obj, urdf_input, env_input, eye_xs, eye_ys, eye_zs, tar_y
 
                             door_name=os.path.basename(urdf_input.replace('.urdf', ''))
                             env_name=os.path.basename(env_input.replace('.urdf',''))
-
+                            
+                            #Name of the img/json files
                             sample_name=str(env_name+"_"+door_name+"_ex_" + str('%.4s'%eye_x) + "_ey_" + str('%.4s'%eye_y) + "_ez_" + str(
                                     '%.4s'%eye_z) + "_ty_" + str('%.4s'%tar_y) + "_tz_" + str('%.4s'%tar_z)+ "_da_" + str('%.4s'%door_angle)+".FORMAT")
 
@@ -281,7 +305,7 @@ def generate_data_imgs(obj, urdf_input, env_input, eye_xs, eye_ys, eye_zs, tar_y
                                              =height)]
 
 
-                            #axis_img = [clip_axis_point(axis_img[0],axis_img[1],400,400), clip_axis_point(axis_img[1],axis_img[0],400,400)]#clip_to_im(point_2d=axis_img, imwidth=width, imheight=height)
+                            # Create datapoint if image fulfills quality criteria
                             if good_image(plane_bb_img=plane_bb_im, handle_bb_img=img_handle, axis_img=axis_img, imwidth=width, imheight=height, min_ec=0.5, ec_weight=1.0,
                                           iou_weight=1.0, min_score=0.7):
                                 generate_datapoint(sample_name,
@@ -290,7 +314,8 @@ def generate_data_imgs(obj, urdf_input, env_input, eye_xs, eye_ys, eye_zs, tar_y
                                                    rotation=[clip_axis_point(axis_img[0],axis_img[1],400,400),clip_axis_point(axis_img[1],axis_img[0],400,400)], axis_is_right=axis_is_right, json_path=None,
                                                    rgb_img=rgbImg, depth_img=depthImg,
                                                    seg_img=segImg)
-
+                                
+# Returning 2D bb in the following format [[min_x, min_y], [max_x, max_y]]
 def get_min_max_bb(bb_in, buffer_hor=0, buffer_ver=0):
     max_x = max(bb_in[0][0], bb_in[1][0]) + buffer_hor
     min_x = min(bb_in[0][0], bb_in[1][0]) - buffer_hor
@@ -298,24 +323,8 @@ def get_min_max_bb(bb_in, buffer_hor=0, buffer_ver=0):
     min_y = min(bb_in[0][1], bb_in[1][1]) - buffer_ver
     return [[min_x, min_y], [max_x, max_y]]
 
-
-def clip_to_im(point_2d, imwidth, imheight):
-    point_2d[0][0] = int(np.clip(point_2d[0][0], 0, imwidth))
-    point_2d[1][0] = int(np.clip(point_2d[1][0], 0, imwidth))
-    point_2d[0][1] = int(np.clip(point_2d[0][1], 0, imheight))
-    point_2d[1][1] = int(np.clip(point_2d[1][1], 0, imheight))
-    return point_2d
-
-def get_axis_intersection(axis):
-    line1=LineString([[10,10],[0,0]])
-    line2=LineString([[0,8],[10,0]])
-
-    int_pt=line1.intersection(line2)
-    if line1.crosses(line2):
-        point_of_intersection= int_pt.x, int_pt.y
-        print(point_of_intersection)
-    else: print("Doesn't cross!!!")
-
+# Returns point clipped to img dims. target_point determines, 
+# where the axis is heading to determine in which direction it points
 def clip_axis_point(point, target_point, imwidth, imheight):
     if point[0]>=0 and point[0]<imwidth and point[1]>=0 and point[1]<imheight:
         return point
@@ -363,27 +372,14 @@ def main(*argv):
     # Get rotation axis
     #axis_is_left, axis=get_rotation_axis(plane_bb=plane_bb, handle_bb=handle_bb)
     #drawAABB(axis)
-
-    """eye_xs = np.linspace(3, 1, 3)
-    eye_ys = np.linspace(-1.0, 1.0, 5)
-    eye_zs = np.linspace(0.5, 2.0, 4)
-
-    tar_ys = np.linspace(-1.0, 1.0, 5)
-    tar_zs = np.linspace(0.0, 2, 5)"""
+    
+    # Sets target positions/orientations of camera
     eye_xs = np.linspace(3, 1, 3)
     eye_ys = np.linspace(-1.0, 1.0, 3)
     eye_zs = np.linspace(0.5, 2.0, 3)
 
     tar_ys = np.linspace(-1.0, 1.0, 4)
     tar_zs = np.linspace(0.0, 2, 3)
-    """eye_xs = np.linspace(3, 1, 2)
-    eye_ys = np.linspace(-0.5, 0.5, 2)
-    eye_zs = np.linspace(0.5, 2.0, 2)
-
-    tar_ys = np.linspace(-0.5, 0.5, 2)
-    tar_zs = np.linspace(0.0, 2, 2)"""
-
-
 
     generate_data_imgs(obj=obj, urdf_input=argv[0], env_input=argv[1],
                        eye_xs=eye_xs, eye_ys=eye_ys, eye_zs=eye_zs, tar_ys=tar_ys, tar_zs=tar_zs, door_angles=[0.0])
